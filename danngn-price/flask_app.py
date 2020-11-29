ff47e29a20d9922d2f4f05198d2cb7fc2cfeeedb
hunmin-hub <hakddal707@naver.com>
2020-11-29 18:16:11 +0900

    20201129
    당근마켓 시세검색

--- danngn-price/flask_app.py
+++ danngn-price/flask_app.py
@@ -4,17 +4,16 @@ import main as proc
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    #print(proc.scrapping_market("플스4",4))
+def index():
     return render_template("index.html")
 
-@app.route('/post',methods=['POST'])
+@app.route('/search',methods=['POST'])
 def search():
-    product=request.form['input']
-    button_count=request.form['count']
-    middle_price=proc.scrapping_market(product,int(button_count))
+    product = request.form['input']
+    button_count = request.form['choices-single-defaul']
+    middle_price = proc.scrapping_market(product, int(button_count))
     if middle_price == int(-1):
         return render_template("warning.html")
     else:
-        return render_template("search.html", searchingBy=product,searchingCt=button_count,searchingPrice=middle_price)
+        return render_template("search.html", searchingBy=product, searchingCt=button_count, searchingPrice=middle_price)
 app.run(host="127.0.0.1")
